cb3e6765c9375864d6bcf1e7480403d4732deff6
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-09-01 20:02:42 +0530

    Add files via upload

---
+++ Sem3/Assembly/A1/q5.asm
@@ -0,0 +1,31 @@
+.model small
+.stack 100h
+.data
+.code
+
+main proc
+mov ax,@data
+mov ds,ax
+
+mov si,0030h
+mov di,0050h
+mov cx,0005h
+mov ax,0000h
+mov bx,0000h
+l1:
+mov bl,[si]
+add ax,bx
+adc dl,00h
+inc si
+loop l1
+mov [di],ax
+inc di
+inc di
+mov [di],dl
+
+int 03h
+mov ah,4ch
+int 21h
+
+main endp
+end main
