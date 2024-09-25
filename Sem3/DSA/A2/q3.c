15b4cc1d8073255dc77ca858daf5360566c46f7d
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-09-25 09:25:33 +0530

    Update q3.c

--- Sem3/DSA/A2/q3.c
+++ Sem3/DSA/A2/q3.c
@@ -1,182 +1,180 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
-
-// Define the structure for a doubly linked list node
-typedef struct Node {
-    int data;
-    struct Node* next;
-    struct Node* prev;
-} Node;
-
-// Function to create a new node
-Node* createNode(int data) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev = NULL;
-    return newNode;
+typedef struct node
+{
+    int n;
+    struct node* next;
+    struct node* prev;
+}node;
+node* head=NULL;
+void view(node*);
+void view(node* ptr)
+{
+    printf("head->%d->",ptr->n);
+    ptr=ptr->next;
+    while(ptr!=NULL)
+    {
+        printf("<-%i->",ptr->n);
+        ptr=ptr->next;
+    }
+    printf("NULL");
 }
+int main()
+{
+    int noNodes=0;
+    while(true)
+    {
+        printf("\na. Insert a node at a specified position \nb. View List \nc. Delete a node from a specified position. \nd. Count the number of nodes in the linked list. \ne. Reverse the linked list. \nf. Exit \nEnter choice: ");
+        char ch;
+        scanf(" %c",&ch);
+        switch(ch)
+        {
+            case 'a':
+            if(noNodes==0)
+            {
+                head=(node*)malloc(sizeof(node));
+                printf("n: ");
+                int no;
+                scanf("%i",&no);
+                head->n=no;
+                head->next=NULL;
+                head->prev=NULL;
+                noNodes++;
+            }
+            else
+            {
+                printf("Enter position: ");
+                int pos;
+                scanf("%i",&pos);
+                if(pos>0 && pos<=noNodes+1)
+                {
+                    node* ptr=(node*)malloc(sizeof(node));
+                    printf("n: ");
+                    int no;
+                    scanf("%i",&no);
+                    ptr->n=no;
+                    ptr->next=NULL;
+                    ptr->prev=NULL;
+                    if(pos==1)
+                    {
+                        ptr->next=head;
+                        head->prev=ptr;
+                        head=ptr;
+                    }
+                    else
+                    {
+                        int c=1;
+                        node* p=head;
+                        while(p!=NULL && c<pos-1)
+                        {
+                            p=p->next;
+                            c++;
+                        }
+                        ptr->next=p->next;
+                        if((ptr->next)!=NULL)
+                        (ptr->next)->prev=ptr;
+                        p->next=ptr;
+                        ptr->prev=p;
+                    }
+                    noNodes++;
+                }
+                
+                else{
+                    printf("Invalid position !");
+                }
 
-// Function to insert a node at a specified position
-void insertAtPosition(Node** head, int data, int position) {
-    Node* newNode = createNode(data);
-   
-    // Insert at the head (position 0)
-    if (position == 0) {
-        newNode->next = *head;
-        if (*head != NULL) {
-            (*head)->prev = newNode;
+            }
+            break;
+
+            case 'b':
+            if(noNodes>0)
+            view(head);
+            else
+            printf("Emtpy list ! Invalid request");
+            break;
+
+            case 'c':
+            if(noNodes>0)
+            {
+                printf("Enter position: ");
+                int pos;
+                scanf("%i",&pos);
+                if(pos==1)
+                {
+                    node* p=head;
+                    head=p->next;
+                    p->next=NULL;
+                    head->prev=NULL;
+                    free(p);
+                    noNodes--;
+                }
+                else if(pos>1 && pos<=noNodes)
+                {
+                    int c=1;
+                    node* p=head;
+                    while(p!=NULL && c<pos-1)
+                    {
+                        p=p->next;
+                        c++;
+                    }
+                    node* pre=p;
+                    p=p->next;
+                    pre->next=p->next;
+                    if((p->next)!=NULL)
+                    (p->next)->prev=pre;
+                    p->next=NULL;
+                    p->prev=NULL;
+                    free(p);
+                    noNodes--;
+                }
+                else
+                printf("Invalid position");
+            }
+            else
+            printf("Emtpy list ! Invalid request");
+            break;
+
+            case 'd':
+            printf("Nodes in linked list: %i",noNodes);
+            break;
+
+            case 'e':
+            if(noNodes==0)
+            printf("Emtpy list ! Invalid request");
+            else if(noNodes==1)
+            break;
+            else
+            {
+                node* pre;
+                node* curr;
+                node* nxt;
+                pre=head;
+                curr=head->next;
+                nxt=curr->next;
+                pre->next=NULL;
+                while(nxt!=NULL)
+                {
+                    curr->next=pre;
+                    curr->prev=NULL;
+                    nxt->prev=NULL;
+                    pre->prev=curr;
+                    pre=curr;
+                    curr=nxt;
+                    nxt=nxt->next;
+                }
+                curr->next=pre;
+                pre->prev=curr;
+                pre=curr;
+                head=pre;
+
+            }
+            break;
+
+            case 'f':
+            return 0;
         }
-        *head = newNode;
-        return;
+
     }
-
-    Node* current = *head;
-    for (int i = 0; current != NULL && i < position - 1; i++) {
-        current = current->next;
-    }
-
-    if (current == NULL) {
-        printf("Position out of range\n");
-        free(newNode);
-        return;
-    }
-
-    newNode->next = current->next;
-    if (current->next != NULL) {
-        current->next->prev = newNode;
-    }
-    current->next = newNode;
-    newNode->prev = current;
-}
-
-// Function to delete a node from a specified position
-void deleteAtPosition(Node** head, int position) {
-    if (*head == NULL) {
-        printf("List is empty\n");
-        return;
-    }
-
-    Node* temp = *head;
-
-    // Delete from the head (position 0)
-    if (position == 0) {
-        *head = temp->next;
-        if (*head != NULL) {
-            (*head)->prev = NULL;
-        }
-        free(temp);
-        return;
-    }
-
-    for (int i = 0; temp != NULL && i < position; i++) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Position out of range\n");
-        return;
-    }
-
-    if (temp->prev != NULL) {
-        temp->prev->next = temp->next;
-    }
-    if (temp->next != NULL) {
-        temp->next->prev = temp->prev;
-    }
-    free(temp);
-}
-
-// Function to count the number of nodes in the doubly linked list
-int countNodes(Node* head) {
-    int count = 0;
-    Node* current = head;
-    while (current != NULL) {
-        count++;
-        current = current->next;
-    }
-    return count;
-}
-
-// Function to reverse print the doubly linked list
-void reversePrint(Node* head) {
-    if (head == NULL) return;
-
-    // Go to the end of the list
-    Node* current = head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    // Print in reverse
-    while (current != NULL) {
-        printf("%d <- ", current->data);
-        current = current->prev;
-    }
-    printf("NULL\n");
-}
-
-// Function to reverse the doubly linked list
-void reverseList(Node** head) {
-    Node *temp = NULL;
-    Node *current = *head;
-
-    // Swap next and prev for all nodes of the list
-    while (current != NULL) {
-        temp = current->prev;
-        current->prev = current->next;
-        current->next = temp;
-        current = current->prev;
-    }
-
-    // Update the head to the new head
-    if (temp != NULL) {
-        *head = temp->prev;
-    }
-}
-
-// Function to print the doubly linked list
-void printList(Node* head) {
-    Node* current = head;
-    while (current != NULL) {
-        printf("%d <-> ", current->data);
-        current = current->next;
-    }
-    printf("NULL\n");
-}
-
-// Main function to test the doubly linked list operations
-int main() {
-    Node* head = NULL;
-
-    // Insert nodes
-    insertAtPosition(&head, 10, 0);
-    insertAtPosition(&head, 20, 1);
-    insertAtPosition(&head, 30, 2);
-    insertAtPosition(&head, 25, 2);
-    printf("List after insertions: ");
-    printList(head);
-
-    // Count nodes
-    printf("Number of nodes: %d\n", countNodes(head));
-
-    // Reverse print the list
-    printf("Reverse print of the list: ");
-    reversePrint(head);
-
-    // Reverse the doubly linked list
-    reverseList(&head);
-    printf("List after reversal: ");
-    printList(head);
-
-    // Delete nodes
-    deleteAtPosition(&head, 1);
-    printf("List after deletion at position 1: ");
-    printList(head);
-
-    deleteAtPosition(&head, 0);
-    printf("List after deletion at position 0: ");
-    printList(head);
-
     return 0;
 }
