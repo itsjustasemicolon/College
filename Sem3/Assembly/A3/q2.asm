c42f61087c1c789add37d27118a76d4d19961ef5
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-11-09 14:56:00 +0530

    Create q2.asm

---
+++ Sem3/Assembly/A3/q2.asm
@@ -0,0 +1,25 @@
+.model small
+.stack 100h
+.code
+main proc
+mov ax, @data
+mov ds, ax
+mov si, 0030h
+mov al, [si]
+mov cl, 06h
+inc si
+l1:
+cmp al, [si]
+jnc l2
+mov al, [si]
+l2:
+inc si
+loop l1
+mov si,0040h
+mov [si], al
+int 06h
+mov ah,4ch
+int 21h
+main endp
+end main
+
