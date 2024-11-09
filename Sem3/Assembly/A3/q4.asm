e09e542dceb3bab64202c29fbff0f60066ba0f46
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-11-09 14:57:29 +0530

    Create q4.asm

---
+++ Sem3/Assembly/A3/q4.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.code
+main proc
+mov ax, @data
+mov ds, ax
+mov bl, 06h
+l3:
+mov si,0030h
+mov cl,06h
+l1:
+mov al,[si]
+inc si
+cmp al,[si]
+jnc l2
+mov dl,[si]
+mov [si],al
+dec si
+mov [si],al
+inc si
+l2:
+loop l1
+dec bl
+cmp bl,00h
+jnz l3
+int 03h
+mov ah,4ch
+int 21h
+main endp
+end main
