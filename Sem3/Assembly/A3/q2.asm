df5f33028aa514117b352f16678d25c1aa740352
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-11-09 15:32:33 +0530

    Update q2.asm

--- Sem3/Assembly/A3/q2.asm
+++ Sem3/Assembly/A3/q2.asm
@@ -1,25 +1,29 @@
 .model small
 .stack 100h
+.data
 .code
 main proc
-mov ax, @data
-mov ds, ax
-mov si, 0030h
-mov al, [si]
-mov cl, 06h
-inc si
-l1:
-cmp al, [si]
-jnc l2
-mov al, [si]
-l2:
-inc si
-loop l1
-mov si,0040h
-mov [si], al
-int 06h
-mov ah,4ch
-int 21h
-main endp
-end main
+	mov ax, @data
+	mov ds, ax
+	mov si, 0030h
 
+	mov al, 00h
+	mov cx, 0007h
+
+l1:
+	cmp al, [si]
+	jnc l2
+	mov al, [si]
+
+l2:
+	inc si
+	loop l1
+
+mov si, 0040h
+mov [si], al
+
+int 03h
+
+main endp 
+end main
+	
