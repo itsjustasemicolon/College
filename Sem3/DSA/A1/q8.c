dbc2ab98f6a658aa1c6f028d2bc506d0c3ba86c5
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2024-12-19 22:28:04 +0530

    Update q8.c

--- Sem3/DSA/A1/q8.c
+++ Sem3/DSA/A1/q8.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+    int row;
+    int col;
+    int val;
+} Element;
+
 
 int main() {
-    int rows, cols,nonZeroCount;
-    // Input the dimensions of the matrix
-    int i,j;
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
-    printf("Enter the number of columns: ");
-    scanf("%d", &cols);
+    
+	printf("Enter no of elements of matrix ");
+	int s;
+	int i=0,rmax=-1,j=0;
+	scanf("%d",&s);
+    Element mat1[s];
+	printf("Enter rol,col,val of Matrix\n");
+    for(i=0;i<s;i++)
+	{
+		int r,c,v;
+		scanf("%d %d %d",&r,&c,&v);
+		mat1[i].row=r;
+		mat1[i].col=c;
+		mat1[i].val=v;
+		if(r>rmax)
+		rmax=r;
+	}
 
-    int matrix[rows][cols];
-
-    // Input the elements of the matrix
-    printf("Enter the elements of the matrix:\n");
-    for ( i = 0; i < rows; i++) {
-        for ( j = 0; j < cols; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-
-    // Find and print the number of non-zero elements in each row
-    printf("Number of non-zero elements in each row:\n");
-    for ( i = 0; i < rows; i++) {
-         nonZeroCount = 0;
-        for ( j = 0; j < cols; j++) {
-            if (matrix[i][j] != 0) {
-                nonZeroCount++;
-            }
-        }
-        printf("Row %d: %d non-zero elements\n", i , nonZeroCount);
-    }
-
+	for(i=0;i<=rmax;i++)
+	{
+		int c=0;
+		for(j=0;j<s;j++)
+		{
+			if(mat1[j].row==i && mat1[j].val==0)
+			c++;
+		}
+		if(c!=0)
+		printf("No. of zeroes in row %d is %d\n",i,c);
+	}
+    
     return 0;
 }
-
