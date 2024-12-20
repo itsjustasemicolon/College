7b0abcce8722dbc5081a8d26ecffdee067da53e6
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2024-12-20 14:10:13 +0530

    Update q2.c

--- Sem3/DSA/A1/q2.c
+++ Sem3/DSA/A1/q2.c
@@ -4,19 +4,16 @@ int main (){
   
     int n, i,j;
 
-    // Input the number of elements
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Input the elements into the array
     printf("Enter the elements: \n");
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Reverse the array
     printf("Reversed array: \n");
     i=0;
     j=n-1;
