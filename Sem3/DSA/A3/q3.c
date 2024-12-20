bb204d224a8cf8382edb87c0a40d84219e058e1d
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2024-12-20 15:19:49 +0530

    Update q3.c

--- Sem3/DSA/A3/q3.c
+++ Sem3/DSA/A3/q3.c
@@ -1,195 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-};
+typedef struct Node {
+    int key;
+    struct Node *left, *right;
+} Node;
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
+Node* createNode(int key) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
     newNode->left = newNode->right = NULL;
     return newNode;
 }
 
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) {
-        root = createNode(data);
-        return root;
-    }
-
-    char direction;
-    printf("Insert left (l) or right (r) of %d? ", root->data);
-    scanf(" %c", &direction);
-
-    if (direction == 'l') {
-        root->left = insert(root->left, data);
-    } else if (direction == 'r') {
-        root->right = insert(root->right, data);
-    } else {
-        printf("Invalid choice\n");
-    }
-
+Node* insert(Node* root, int key) {
+    if (root == NULL)
+        return createNode(key);
+    if (key < root->key)
+        root->left = insert(root->left, key);
+    else if (key > root->key)
+        root->right = insert(root->right, key);
     return root;
 }
 
-struct Node* search(struct Node* root, int key) {
-    if (root == NULL || root->data == key) {
+Node* search(Node* root, int key) {
+    if (root == NULL || root->key == key)
         return root;
-    }
-    if (key < root->data) {
+    if (key < root->key)
         return search(root->left, key);
-    }
     return search(root->right, key);
 }
 
-void inOrder(struct Node* root) {
-    if (root == NULL) {
-        return;
-    }
-    inOrder(root->left);
-    printf("%d ", root->data);
-    inOrder(root->right);
-}
-
-struct Node* findMin(struct Node* root) {
-    while (root && root->left != NULL) {
+Node* findMin(Node* root) {
+    while (root && root->left)
         root = root->left;
-    }
     return root;
 }
 
-struct Node* findMax(struct Node* root) {
-    while (root && root->right != NULL) {
-        root = root->right;
-    }
-    return root;
-}
-
-void findPredecessorSuccessor(struct Node* root, int key, struct Node** predecessor, struct Node** successor) {
-    if (root == NULL) {
-        return;
-    }
-
-    if (root->data == key) {
-        if (root->left != NULL) {
-            *predecessor = findMax(root->left);
-        }
-
-        if (root->right != NULL) {
-            *successor = findMin(root->right);
-        }
-        return;
-    }
-
-    if (key < root->data) {
-        *successor = root;  
-        findPredecessorSuccessor(root->left, key, predecessor, successor);
-    } else {
-        *predecessor = root;  
-        findPredecessorSuccessor(root->right, key, predecessor, successor);
-    }
-}
-
-struct Node* deleteNode(struct Node* root, int key) {
-    if (root == NULL) {
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL)
         return root;
-    }
-
-    if (key < root->data) {
+    if (key < root->key)
         root->left = deleteNode(root->left, key);
-    } else if (key > root->data) {
+    else if (key > root->key)
         root->right = deleteNode(root->right, key);
-    } else {
+    else {
         if (root->left == NULL) {
-            struct Node* temp = root->right;
+            Node* temp = root->right;
             free(root);
             return temp;
         } else if (root->right == NULL) {
-            struct Node* temp = root->left;
+            Node* temp = root->left;
             free(root);
             return temp;
         }
-
-        struct Node* temp = findMin(root->right);
-        root->data = temp->data;
-        root->right = deleteNode(root->right, temp->data);
+        Node* temp = findMin(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
     }
     return root;
 }
 
+void findPreSuc(Node* root, Node** pre, Node** suc, int key) {
+    if (root == NULL)
+        return;
+    if (root->key == key) {
+        if (root->left != NULL) {
+            Node* temp = root->left;
+            while (temp->right)
+                temp = temp->right;
+            *pre = temp;
+        }
+        if (root->right != NULL) {
+            Node* temp = root->right;
+            while (temp->left)
+                temp = temp->left;
+            *suc = temp;
+        }
+        return;
+    }
+    if (key < root->key) {
+        *suc = root;
+        findPreSuc(root->left, pre, suc, key);
+    } else {
+        *pre = root;
+        findPreSuc(root->right, pre, suc, key);
+    }
+}
+
+void inorder(Node* root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->key);
+    inorder(root->right);
+}
+
 int main() {
-    struct Node* root = NULL;
+    Node* root = NULL;
     int choice, key;
-    struct Node *predecessor, *successor;
+    Node *pre, *suc;
 
     while (1) {
-        printf("\nMenu:\n");
-        printf("1. Create/Insert a node\n");
-        printf("2. Search for a key\n");
-        printf("3. Find In-order Predecessor and Successor of a key\n");
-        printf("4. Delete a key\n");
-        printf("5. In-order Traversal\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
+        printf("\n1. Create/Insert\n2. Search\n3. Find In-order Predecessor and Successor\n4. Insert Key\n5. Delete Key\n6. Display In-order\n7. Exit\nEnter your choice: ");
         scanf("%d", &choice);
-
         switch (choice) {
             case 1:
-                printf("Enter the key to insert: ");
+                printf("Enter key to insert: ");
                 scanf("%d", &key);
                 root = insert(root, key);
-                printf("Key inserted successfully!\n");
                 break;
-
             case 2:
-                printf("Enter the key to search: ");
+                printf("Enter key to search: ");
                 scanf("%d", &key);
-                if (search(root, key)) {
-                    printf("Key %d found in the tree.\n", key);
-                } else {
-                    printf("Key %d not found in the tree.\n", key);
-                }
+                Node* found = search(root, key);
+                if (found)
+                    printf("Key found: %d\n", found->key);
+                else
+                    printf("Key not found\n");
                 break;
-
             case 3:
-                printf("Enter the key to find predecessor and successor: ");
+                printf("Enter key: ");
                 scanf("%d", &key);
-                predecessor = NULL;
-                successor = NULL;
-                findPredecessorSuccessor(root, key, &predecessor, &successor);
-                if (predecessor) {
-                    printf("Predecessor of %d is %d\n", key, predecessor->data);
-                } else {
-                    printf("No Predecessor of %d\n", key);
-                }
-                if (successor) {
-                    printf("Successor of %d is %d\n", key, successor->data);
-                } else {
-                    printf("No Successor of %d\n", key);
-                }
+                pre = suc = NULL;
+                findPreSuc(root, &pre, &suc, key);
+                if (pre)
+                    printf("Predecessor: %d\n", pre->key);
+                else
+                    printf("No Predecessor\n");
+                if (suc)
+                    printf("Successor: %d\n", suc->key);
+                else
+                    printf("No Successor\n");
                 break;
-
             case 4:
-                printf("Enter the key to delete: ");
+                printf("Enter key to insert: ");
+                scanf("%d", &key);
+                root = insert(root, key);
+                break;
+            case 5:
+                printf("Enter key to delete: ");
                 scanf("%d", &key);
                 root = deleteNode(root, key);
-                printf("Key %d deleted from the tree.\n", key);
                 break;
-
-            case 5:
-                printf("In-order traversal of the tree: ");
-                inOrder(root);
+            case 6:
+                printf("In-order traversal: ");
+                inorder(root);
                 printf("\n");
                 break;
-
-            case 6:
+            case 7:
                 exit(0);
-
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Invalid choice\n");
         }
     }
     return 0;
