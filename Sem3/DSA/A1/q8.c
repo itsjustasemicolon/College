a177c8be8418fc81c94eaccee76acec857dd3610
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2024-12-20 10:41:45 +0530

    Update q8.c

--- Sem3/DSA/A1/q8.c
+++ Sem3/DSA/A1/q8.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct {
+
+typedef struct
+{
     int row;
     int col;
     int val;
-} Element;
+} Matrix;
 
+int main()
+{
+    printf("Enter the number of elements in the matrix: ");
+    int s;
+    int i = 0, rmax = -1, j = 0;
+    scanf("%d", &s);
+    Matrix mat1[s];
+    for (i = 0; i < s; i++)
+    {
+    printf("Enter row, col, val of the matrix:\n");
+        int r, c, v;
+        scanf("%d %d %d", &r, &c, &v);
+        mat1[i].row = r;
+        mat1[i].col = c;
+        mat1[i].val = v;
+        if (r > rmax)
+            rmax = r;
+    }
 
-int main() {
-    
-	printf("Enter no of elements of matrix ");
-	int s;
-	int i=0,rmax=-1,j=0;
-	scanf("%d",&s);
-    Element mat1[s];
-	printf("Enter rol,col,val of Matrix\n");
-    for(i=0;i<s;i++)
-	{
-		int r,c,v;
-		scanf("%d %d %d",&r,&c,&v);
-		mat1[i].row=r;
-		mat1[i].col=c;
-		mat1[i].val=v;
-		if(r>rmax)
-		rmax=r;
-	}
+    // Find the number of non-zero elements in each row
+    for (i = 0; i <= rmax; i++)
+    {
+        int count = 0;
+        for (j = 0; j < s; j++)
+        {
+            if (mat1[j].row == i && mat1[j].val != 0)
+                count++;
+        }
+        if (count != 0)
+            printf("No. of non-zero elements in row %d is %d\n", i, count);
+    }
 
-	for(i=0;i<=rmax;i++)
-	{
-		int c=0;
-		for(j=0;j<s;j++)
-		{
-			if(mat1[j].row==i && mat1[j].val==0)
-			c++;
-		}
-		if(c!=0)
-		printf("No. of zeroes in row %d is %d\n",i,c);
-	}
-    
     return 0;
 }
